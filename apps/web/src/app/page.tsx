33b5b185d62e157f61381c990a6d4812ba511dbd
Diogo de Souza <diogo@sozua.dev>
2024-04-23 03:30:48 -0300

    first commit

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main>
+      <div className="max-w-screen-xl w-full mx-auto">
+        <h2 className="text-lg font-bold tracking-tight">Brags</h2>
+      </div>
+    </main>
+  );
+}
